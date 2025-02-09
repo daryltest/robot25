abcafabad48ded11a936761fe6cbd1960f0c998c
daryltest <105012312+daryltest@users.noreply.github.com>
2025-02-09 10:48:33 -0500

    Create track2.py

---
+++ python_try_1/track2.py
@@ -0,0 +1,23 @@
+from picamera2 import Picamera2
+import time
+import cv2
+
+picam2 = Picamera2()
+config = picam2.create_still_configuration(main= {"size": (1640, 1232)}, lores = {"size": (410, 308)}, display = "lores", buffer_count = 3, queue = False)
+picam2.configure(config)
+
+picam2.set_controls({"ExposureTime": 10000, "AnalogueGain": 5}) #Shutter time and analogue signal boost
+picam2.start(show_preview=True)
+
+time.sleep(10)  #enjoy the preview
+
+t_0 = time.monotonic()
+img = picam2.capture_array("lores") #this takes a picture. img can be used with cv2
+t_1 = time.monotonic()
+picam2.close() #when you're done taking photos, this closes the camera connection
+
+print("taking the photo took %s seconds", round(t_1-t_0, 3))
+print("width height\t:", *img.shape[0:2][::-1])
+
+cv2.imshow("Title", cv2.resize(img, (0,0), fx=0.25, fy=0.25)) #resize the image to a quarter of the original size
+cv2.waitKey(0) #Wait until a key is pressed while the img window is selected
