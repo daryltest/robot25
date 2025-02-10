5081b096b940575c6e45bc3f3618529d9e177e0a
daryltest <105012312+daryltest@users.noreply.github.com>
2025-02-10 16:57:36 -0500

    Update track3.py

--- python_try_1/track3.py
+++ python_try_1/track3.py
@@ -41,28 +41,27 @@ while True:
 	# (x, y) center of the ball
 	cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
 		cv2.CHAIN_APPROX_SIMPLE)
-	print(f"clen {len(cnts)}")
 	cnts = cnts[0]
 	center = None
 
-	# # only proceed if at least one contour was found
-	# if len(cnts) > 0:
-	# 	# find the largest contour in the mask, then use
-	# 	# it to compute the minimum enclosing circle and
-	# 	# centroid
-	# 	c = max(cnts, key=cv2.contourArea)
-	# 	((x, y), radius) = cv2.minEnclosingCircle(c)
+	# only proceed if at least one contour was found
+	if len(cnts) > 0:
+		# find the largest contour in the mask, then use
+		# it to compute the minimum enclosing circle and
+		# centroid
+		c = max(cnts, key=cv2.contourArea)
+		((x, y), radius) = cv2.minEnclosingCircle(c)
 
-	# 	M = cv2.moments(c)
-	# 	center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+		M = cv2.moments(c)
+		center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
 
-	# 	# only proceed if the radius meets a minimum size
-	# 	if radius > 3:
-	# 		# draw the circle and centroid on the frame,
-	# 		# then update the list of tracked points
-	# 		cv2.circle(frame, (int(x), int(y)), int(radius),
-	# 			(0, 255, 255), 2)
-	# 		cv2.circle(frame, center, 5, (0, 0, 255), -1)
+		# only proceed if the radius meets a minimum size
+		if radius > 3:
+			# draw the circle and centroid on the frame,
+			# then update the list of tracked points
+			cv2.circle(frame, (int(x), int(y)), int(radius),
+				(255, 255, 0), 2)
+			cv2.circle(frame, center, 5, (255, 0, 0), -1)
 
 	# show the frame to our screen
 	cv2.imshow("track3", frame)
