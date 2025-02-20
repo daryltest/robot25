c985c6ed7278168438fa8a4b6603ee6461407c14
daryltest <105012312+daryltest@users.noreply.github.com>
2025-02-20 18:39:42 -0500

    Update track3.py -- test images

--- python_try_1/track3.py
+++ python_try_1/track3.py
@@ -9,7 +9,8 @@ targetLower = (111, 210, 30)
 targetUpper = (121, 255, 255)
 
 picam2 = Picamera2()
-config = picam2.create_still_configuration(main = {"size": (820, 616), "format": "RGB888"})
+#config = picam2.create_still_configuration(main = {"size": (820, 616), "format": "RGB888"})
+config = picam2.create_still_configuration(main = {"size": (1640, 1232), "format": "RGB888"})
 picam2.configure(config)
 picam2.set_controls({"AnalogueGain": 2.0})
 
@@ -37,49 +38,50 @@ while True:
 		break
 	print(f"got frame {frame.shape}")
 
-	#frame = cv2.resize(frame, (410, 308), interpolation=cv2.INTER_AREA)
-	blurred = cv2.GaussianBlur(frame, (11, 11), 0)
-	hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
+	frame = cv2.resize(frame, (410, 308), interpolation=cv2.INTER_AREA)
+	if false:
+		blurred = cv2.GaussianBlur(frame, (11, 11), 0)
+		hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
 	
-	# construct a mask for the color "green", then perform
-	# a series of dilations and erosions to remove any small
-	# blobs left in the mask
-	mask = cv2.inRange(hsv, targetLower, targetUpper)
-#	masked = cv2.bitwise_and(frame, frame, mask=mask)
-
-	mask = cv2.erode(mask, None, iterations=2)
-	mask = cv2.dilate(mask, None, iterations=2)
-
-	#cv2.imshow("rgb", frame)
-	#cv2.imshow("hsv", hsv)
-	#cv2.imshow("mask", mask)
-
-	# find contours in the mask and initialize the current
-	# (x, y) center of the ball
-	cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
-		cv2.CHAIN_APPROX_SIMPLE)
-	cnts = cnts[0]
-	center = None
-
-	# only proceed if at least one contour was found
-	if len(cnts) > 0:
-		# find the largest contour in the mask, then use
-		# it to compute the minimum enclosing circle and
-		# centroid
-		c = max(cnts, key=cv2.contourArea)
-		((x, y), radius) = cv2.minEnclosingCircle(c)
-
-		M = cv2.moments(c)
-		center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
-
-		# only proceed if the radius meets a minimum size
-		if radius > 3:
-			# draw the circle and centroid on the frame,
-			# then update the list of tracked points
-			cv2.circle(frame, (int(x), int(y)), int(radius),
-				(255, 255, 0), 2)
-			cv2.circle(frame, center, 5, (255, 0, 0), -1)
-			
+		# construct a mask for the target color, then perform
+		# a series of dilations and erosions to remove any small
+		# blobs left in the mask
+		mask = cv2.inRange(hsv, targetLower, targetUpper)
+		# masked = cv2.bitwise_and(frame, frame, mask=mask)
+	
+		mask = cv2.erode(mask, None, iterations=2)
+		mask = cv2.dilate(mask, None, iterations=2)
+	
+		#cv2.imshow("rgb", frame)
+		#cv2.imshow("hsv", hsv)
+		#cv2.imshow("mask", mask)
+	
+		# find contours in the mask and initialize the current
+		# (x, y) center of the ball
+		cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
+			cv2.CHAIN_APPROX_SIMPLE)
+		cnts = cnts[0]
+		center = None
+	
+		# only proceed if at least one contour was found
+		if len(cnts) > 0:
+			# find the largest contour in the mask, then use
+			# it to compute the minimum enclosing circle and
+			# centroid
+			c = max(cnts, key=cv2.contourArea)
+			((x, y), radius) = cv2.minEnclosingCircle(c)
+	
+			M = cv2.moments(c)
+			center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+	
+			# only proceed if the radius meets a minimum size
+			if radius > 3:
+				# draw the circle and centroid on the frame,
+				# then update the list of tracked points
+				cv2.circle(frame, (int(x), int(y)), int(radius),
+					(255, 255, 0), 2)
+				cv2.circle(frame, center, 5, (255, 0, 0), -1)
+				
 	cv2.imshow("rgb", frame)
 			
 	key = cv2.waitKey(1) & 0xFF
